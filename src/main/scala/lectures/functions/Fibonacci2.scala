e9d9ba3e859245cff3c37560c6c201cbe01d7dc3
Ivan Malyshev <vanya-grozny@yandex.ru>
2017-09-20 14:57:40 +0300

    implement fibsImpl

--- src/main/scala/lectures/functions/Fibonacci2.scala
+++ src/main/scala/lectures/functions/Fibonacci2.scala
@@ -1,5 +1,7 @@
 package lectures.functions
 
+import scala.annotation.tailrec
+
 /**
   * Цель упражнения: используя приемы динамического программирования,
   * реализовать более оптимальный алгоритм подсчета чисел фибоначчи
@@ -16,10 +18,17 @@ object Fibonacci2 extends App {
     if (num <= 3) Array(1, 1, 2)(num - 1)
     else fibsImpl(num, Array(1, 1, 2))(num - 1)
 
+  @tailrec
   private def fibsImpl(num: Int, acc: Array[Int]): Array[Int] = {
-
+    val n = acc.length
+    if (n < num){
+      val newAcc = acc :+ acc(n-1)+acc(n-2)
+      fibsImpl (num, newAcc)
+    }else{
+      acc
+    }
   }
 
   println(fibs2(16))
-  //println(fibs(1000))
+  println(fibs2(1000))
 }
