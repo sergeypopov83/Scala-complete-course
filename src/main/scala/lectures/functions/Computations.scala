d7bbfd54d03fbd498f46b91230711bb26411e452
Ivan Malyshev <vanya-grozny@yandex.ru>
2017-09-20 16:24:22 +0300

    curried Computation complete

--- src/main/scala/lectures/functions/Computations.scala
+++ src/main/scala/lectures/functions/Computations.scala
@@ -41,12 +41,14 @@ object Computation extends App with Data {
   */
 object CurriedComputation extends App with Data {
 
-  def curriedComputation(filterData: String)(dataProducer: Array[String]): Array[String] = ???
+  def curriedComputation(filterData: String)(dataProducer: Array[String]): Array[String] =
+    Computation.computation(filterData, dataProducer)
 
-  val partiallyAppliedCurriedFunction = ???
 
-  //val result = partiallyAppliedCurriedFunction(dataArray)
-  //result.foreach(println)
+  val partiallyAppliedCurriedFunction = curriedComputation (filterData) _
+
+  val result = partiallyAppliedCurriedFunction(dataArray)
+  result.foreach(println)
 }
 
 /**
