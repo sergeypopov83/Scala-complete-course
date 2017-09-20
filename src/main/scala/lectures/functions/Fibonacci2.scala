25c59f8707b0b3b4782b72a603f0a9647979cd50
Ivan Malyshev <vanya-grozny@yandex.ru>
2017-09-20 14:08:41 +0300

    move Fibbonacci2 in the separate file

--- src/main/scala/lectures/functions/Fibonacci2.scala
+++ src/main/scala/lectures/functions/Fibonacci2.scala
@@ -1,5 +1,25 @@
 package lectures.functions
 
-class Fibonacci2 {
+/**
+  * Цель упражнения: используя приемы динамического программирования,
+  * реализовать более оптимальный алгоритм подсчета чисел фибоначчи
+  * Для этого нужно реализовать функцию fibsImpl.
+  * Сигнатуру функции Вы можете расширять по своему усмотрению,
+  * но реализация должна удовлетворять следующим требованиям
+  * * * * метод fibsImpl - должен быть tail recursive
+  * * * * параметр acc - аккумулятор посчитанных значений
+  *
+  */
+object Fibonacci2 extends App {
 
+  def fibs2(num: Int) =
+    if (num <= 3) Array(1, 1, 2)(num - 1)
+    else fibsImpl(num, Array(1, 1, 2))(num - 1)
+
+  private def fibsImpl(num: Int, acc: Array[Int]): Array[Int] = {
+
+  }
+
+  println(fibs2(16))
+  //println(fibs(1000))
 }
