f9c46dac4d01ea201020811f1969c0101a5d7cc1
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-08 16:56:43 +0300

    Complete mergeSort

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -1,5 +1,7 @@
 package lectures.collections
 
+import scala.annotation.tailrec
+
 /**
   * Постарайтесь не использовать мутабильные коллекции и var
   * Подробнее о сортировке можно подсмотреть здесь - https://en.wikipedia.org/wiki/Merge_sort
@@ -8,6 +10,30 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  def mergeSort(data: Seq[Int]): Seq[Int] = data
+  @tailrec
+  def merge(left: Seq[Int], right: Seq[Int], res: Seq[Int] = Seq()): Seq[Int] = {
+    if (left.isEmpty || right.isEmpty)
+      res ++ right ++ left
+    else {
+      if (left(0) < right(0)){
+        merge(left.drop(1), right, res :+ left(0))
+      }else{
+        merge(left, right.drop(1), res :+ right(0))
+      }
+    }
+  }
 
+  def mergeSort(data: Seq[Int]): Seq[Int] = {
+    if (data.size <= 1) data
+    else {
+      val (lToSort, rToSort) = data.splitAt(data.size/2)
+      val left = mergeSort(lToSort)
+      val right = mergeSort(rToSort)
+      merge (left, right)
+    }
+  }
+
+  println(mergeSort(Seq(-5,117, 0, 567, 345, 4, 5, 678, 900)))
+  println(mergeSort(Seq()))
+  println(mergeSort(Seq(-100500)))
 }
