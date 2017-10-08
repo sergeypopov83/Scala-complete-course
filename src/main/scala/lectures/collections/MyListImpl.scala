c0052187558549642e7d342604ee6338131880c8
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-08 14:58:39 +0300

    Complete MyListImpl

--- src/main/scala/lectures/collections/MyListImpl.scala
+++ src/main/scala/lectures/collections/MyListImpl.scala
@@ -19,19 +19,33 @@ object MyListImpl extends App {
 
   case class MyList(data: List[Int]) {
 
-//    def flatMap(f: (Int => MyList)) =
-//      MyList(data.flatMap(inp => f(inp).data))
-//
-//    def map(f: ???) = ???
-//
-//    def foldLeft(acc: Int)(???): Int = ???
-//
-//    def filter(???) = ???
+    def flatMap(f: (Int => MyList)) =
+      MyList(data.flatMap(inp => f(inp).data))
+
+    /** я вообще думал, что flatMap следует
+      * реализовывать через map (ведь map может позволять
+      * создавать List[ List[Int] ]. Но если надо наоборот,
+      * то на ум приходит только один вариант: (Int)=>Int
+      */
+    def map(f: (Int => Int)) =
+      flatMap(inp => MyList(List(f(inp))))
+
+    def foldLeft(acc: Int)(op: (Int,Int) => Int): Int = data match{
+      case Nil => acc
+      case head :: tail => MyList(tail).foldLeft(op(acc,head))(op)
+    }
+
+    def filter(f: (Int) => Boolean) =
+      flatMap{ inp => MyList(
+          if (f(inp)) List(inp)
+          else List[Int]()
+        )
+      }
   }
 
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
-//  require(MyList(Nil).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+  require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
+  require(MyList(List(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((x,y) => x + y) == 21)
+  require(MyList(Nil).foldLeft(0)((x,y) => x + y) == 0)
 
 }
