5e2fcb2f1e0987b97a94d59a836c5fe7b98ec253
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-19 00:05:31 +0300

    Add pattern matching in merge

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -12,28 +12,26 @@ object MergeSortImpl extends App {
 
   @tailrec
   def merge(left: Seq[Int], right: Seq[Int], res: Seq[Int] = Seq()): Seq[Int] = {
-    if (left.isEmpty || right.isEmpty)
-      res ++ right ++ left
-    else {
-      if (left(0) < right(0)){
-        merge(left.drop(1), right, res :+ left(0))
-      }else{
-        merge(left, right.drop(1), res :+ right(0))
-      }
+    (left, right) match {
+      case (Nil, _) => res ++ right
+      case (_, Nil) => res ++ left
+      case (l :: leftTail, r :: rightTail) =>
+        if (l < r) merge(leftTail, right, res :+ l)
+        else merge (left, rightTail, res :+ r)
     }
   }
 
   def mergeSort(data: Seq[Int]): Seq[Int] = {
     if (data.size <= 1) data
     else {
-      val (lToSort, rToSort) = data.splitAt(data.size/2)
+      val (lToSort, rToSort) = data.splitAt(data.size / 2)
       val left = mergeSort(lToSort)
       val right = mergeSort(rToSort)
-      merge (left, right)
+      merge(left, right)
     }
   }
 
-  println(mergeSort(Seq(-5,117, 0, 567, 345, 4, 5, 678, 900)))
+  println(mergeSort(Seq(-5, 117, 0, 567, 345, 4, 5, 678, 900)))
   println(mergeSort(Seq()))
   println(mergeSort(Seq(-100500)))
 }
