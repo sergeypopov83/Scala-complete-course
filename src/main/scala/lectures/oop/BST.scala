f91a604203d70cb0f4bd3e7f0beaa2aaed45916f
Ivan Malyshev <ivan.malyshev@phystech.edu>
2017-10-29 23:51:34 +0300

    toString complete

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -34,8 +34,6 @@ trait BST {
 
   def find(value: Int): Option[BST]
 
-  def dfs(x: Int): Int
-  def tosString(): String
 }
 
 object BSTImpl {
@@ -50,7 +48,7 @@ object BSTImpl {
   }
   private val nodeWidth = 8
 
-  private def emptyStr(n: Int): String = (1 to n).map (_ => " ").mkString("")
+  private def emptyStr(n: Int, s: String = "."): String = (1 to n).map (_ => s).mkString("")
 
   private def iToS(x: Int): String = s"[%${nodeWidth-2}d]".format(x)
 
@@ -101,30 +99,31 @@ case class BSTImpl(value: Int,
     }
   }
 
-  def tosString(): String = {
+  override def toString(): String = {
     case class NodeInQueue(node: BSTImpl, num: Int)
     val depth = this.dfs(1)
     val numOfLeaves = Math.pow(2,depth-1).toInt
     val qu = mutable.Queue(NodeInQueue(this, 1))
-    var resultString = ""
+    var resultString = s"Binary tree (depth = $depth):\n"
     var lastLevel = 0
     var lastPos = 0
 
+    var strToWriteInThisLevel = "";
     while (qu.nonEmpty) {
       val qNode = qu.dequeue()
       val (curLevel, curPos) = getLevelPositionByNum(qNode.num)
-      println (s"  > num: ${qNode.num} ;  lev: $curLevel ;   pos: $curPos")
       val numOfNodes = Math.pow(2,curLevel-1).toInt
       val interval = (numOfLeaves - numOfNodes)*nodeWidth/numOfNodes
       if (curLevel > lastLevel) {
-        resultString += "  endl;\n"
-        lastPos = 0;
+        resultString += "\n%2d:".format(curLevel)
+        lastPos = -1;
         resultString += emptyStr(interval / 2)
-      }
+      } else resultString += strToWriteInThisLevel
 
       for (i <- 1 until (curPos - lastPos))
-        resultString += emptyStr(interval + nodeWidth)
-      resultString += iToS(qNode.node.value) + emptyStr(interval)
+      resultString += emptyStr(nodeWidth, "_") + emptyStr(interval)
+      resultString += iToS(qNode.node.value)
+      strToWriteInThisLevel = emptyStr(interval)
 
       lastPos = curPos
       lastLevel = curLevel
@@ -142,7 +141,7 @@ case class BSTImpl(value: Int,
     resultString
   }
 
-  def dfs(level: Int): Int = {
+  private def dfs(level: Int): Int = {
     (left, right) match {
       case (None, None) => level
       case (Some(lTree), None) => lTree.dfs(level + 1)
@@ -159,7 +158,7 @@ object TreeTest extends App {
 
   val sc = new java.util.Scanner(System.in)
   val maxValue = 110000
-  val nodesCount = 1//sc.nextInt()
+  val nodesCount = sc.nextInt()
 
   val markerItem = 49930//(Math.random() * maxValue).toInt
   val markerItem2 = 92818//(Math.random() * maxValue).toInt
@@ -175,14 +174,11 @@ object TreeTest extends App {
 
   // add marker items
   val testTree = tree.add(markerItem).add(markerItem2).add(markerItem3)
-      .add(30000).add(80000).add(50000).add(1)
-      .add(30001).add(49999).add(60000).add(110000)
 
 //  // check that search is correct
   require(testTree.find(markerItem).isDefined)
   require(testTree.find(markerItem).isDefined)
   require(testTree.find(markerItem).isDefined)
 
-  println(testTree.dfs(1))
-  println(testTree.tosString())
+  println(testTree)
 }
