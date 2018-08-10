fc131ff1e3640c51602298ef7475620174bcdf2a
sergeypopov83@gmail.com <sergeypopov83@gmail.com>
2018-08-10 20:06:47 +0300

    type lambdas example

---
+++ src/main/scala/lectures/types/lambda/TypeLambdaExample.scala
@@ -0,0 +1,24 @@
+package lectures.types.lambda
+import lectures.types.lambda._
+
+
+/**
+  * Объект LeftToOptionProjector превращает все левые проекции Left[A] от Either[A, _] в Some[A], а правые в None
+  *
+  * @tparam F
+  */
+
+abstract class ToOptionProjector[F[_]] {
+  type I[_] = F[_]
+  def projectEither[A](va: I[A]): Option[A]
+}
+
+object LeftToOptionProjector extends ToOptionProjector[({type λ[α] = Either[α, _]})#λ] {
+  def projectEither[A](va: I[A]): Option[A] = va match {
+    case l: Left[A, _] => Option(l.value)
+    case _ => None
+  }
+}
+
+
+
