d05030f5b52672cdb9988e9152443e4349a94371
Anastasia Tomilina <a.i.tomilina@tinkoff.ru>
2018-09-18 23:01:43 +0300

    [hw1] Fibonacci

--- src/main/scala/lectures/functions/Fibonacci.scala
+++ src/main/scala/lectures/functions/Fibonacci.scala
@@ -1,5 +1,7 @@
 package lectures.functions
 
+import scala.annotation.tailrec
+
 /**
   * Цель упражнения: вычислить 9 - е число Фибоначчи
   * Для этого раскомментируйте строчку в методе fibs и исправьте ошибку компиляции.
@@ -12,10 +14,10 @@ package lectures.functions
 object Fibonacci extends App {
 
   // Task 2
-  def fibs(num: Int) = {
+  def fibs(num: Int): Int = {
     if (num == 1) 1
-    if (num == 2) 1
-    //fibs(num - 1) + fibs(num - 2)
+    else if (num == 2) 1
+    else fibs(num - 1) + fibs(num - 2)
   }
 
   println(fibs(9))
@@ -38,7 +40,11 @@ object Fibonacci2 extends App {
     if (num <= 3) Array(1, 1, 2)(num - 1)
     else fibsImpl(num, Array(1, 1, 2))(num - 1)
 
-  private def fibsImpl(num: Int, acc: Array[Int]): Array[Int] = ???
+  @tailrec
+  private def fibsImpl(num: Int, acc: Array[Int]): Array[Int] = {
+    if (acc.length == num) acc
+    else fibsImpl(num, acc :+ acc(acc.length - 1) + acc(acc.length - 2))
+  }
 
   println(fibs2(16))
   //println(fibs(1000))
