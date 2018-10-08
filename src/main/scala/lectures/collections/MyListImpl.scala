ace20c945f1c70611e5fbae1eeff4d7e5bee5005
Anastasia Tomilina <a.i.tomilina@tinkoff.ru>
2018-10-09 00:10:18 +0300

    [hw4] MyListImpl

--- src/main/scala/lectures/collections/MyListImpl.scala
+++ src/main/scala/lectures/collections/MyListImpl.scala
@@ -19,19 +19,24 @@ object MyListImpl extends App {
 
   case class MyList(data: List[Int]) {
 
-//    def flatMap(f: (Int => MyList)) =
-//      MyList(data.flatMap(inp => f(inp).data))
-//
-//    def map(f: ???) = ???
-//
-//    def foldLeft(acc: Int)(???): Int = ???
-//
-//    def filter(???) = ???
+    def flatMap(f: Int => MyList) =
+      MyList(data.flatMap(inp => f(inp).data))
+
+    def map(f: Int => Int) = flatMap(x => MyList(List(f(x))))
+
+    def foldLeft(acc: Int)(f: (Int, Int) => Int): Int = data match {
+      case Nil => acc
+      case head :: tail =>
+        MyList(tail).foldLeft(f(acc, head))(f)
+    }
+
+    def filter(f: Int => Boolean) = flatMap(x => MyList(if (f(x)) List(x) else List()))
+
   }
 
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
-//  require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
-//  require(MyList(Nil).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+  require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
+  require(MyList(List(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl_1, tpl_2) => tpl_1 + tpl_2) == 21)
+  require(MyList(Nil).foldLeft(0)((tpl_1, tpl_2) => tpl_1 + tpl_2) == 0)
 
 }
