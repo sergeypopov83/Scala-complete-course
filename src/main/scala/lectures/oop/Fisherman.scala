b617e1e303e8ea236a457aa85ba10338cf671a01
Anastasia Tomilina <a.i.tomilina@tinkoff.ru>
2018-11-14 10:17:23 +0300

    [hw8] Fisherman

--- src/main/scala/lectures/oop/Fisherman.scala
+++ src/main/scala/lectures/oop/Fisherman.scala
@@ -21,33 +21,31 @@ package lectures.oop
   * * * * так можно: PartD { def str = { val newStr = doSomethingWithD("D"); newStr }}
   * * * * содержимое FishermansApp менять нельзя
   **/
-//??? PartD {
-//  ??? str = "D"
-//}
-//
-//??? PartY {
-//  ??? str = "Y"
-//}
-//
-//??? PartCH {
-//  ??? str = "CH"
-//}
-//
-//??? PartK {
-//  ??? str = "K"
-//}
-//
-//??? PartA {
-//  ??? str = "A"
-//}
-//
-//??? PartO extends PartA {
-//  ??? str = "O"
-//}
-//
-//object FishermansApp extends App {
-//  val o = new PartO
-//  println(o.str)
-//}
+trait PartD extends PartY{
+  override def str: String = super.str + "D"
+}
 
+trait PartY {
+  def str: String = "Y"
+}
 
+trait PartCH extends PartK {
+  override def str: String = "CH" + super.str
+}
+
+trait PartK extends PartA {
+  override def str: String = "K" + super.str
+}
+
+trait PartA {
+  def str = "A"
+}
+
+class PartO extends PartD with PartCH {
+  override def str: String = super[PartD].str + "O" + super[PartCH].str
+}
+
+object FishermansApp extends App {
+  val o = new PartO
+  println(o.str)
+}
